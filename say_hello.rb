edf1f9c1d86b5cd0e25c54fbce1c8beaac73361b
Samuel W Tong <ssamt1142@gmail.com>
2019-01-30 17:18:33 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello (name = "Ruby Programmer")
-  puts "Hello #{name}!"
+def say_hello ("Kent Beck)
+  puts say_hello
 end
 
 def say_hello (name = "Ruby Programmer")
